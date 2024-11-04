3a97f0cc545dce47889c07f44f5fa178cf83e61a
Ygor-Evaldt-dev <evaldtygor@gmail.com>
2024-11-04 08:15:39 -0300

    adiciona descrição sobre aplicação em construção

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,11 @@ export default function Home() {
   return (
     <>
       <h1>Meu Clone do TabNews</h1>
+      <p>
+        Aplicação em contrução, salve este endereço nos favoritos do seu
+        navegador de internet e volte dentro de alguns dias para conferir o
+        resultado =D.
+      </p>
     </>
   );
 }
