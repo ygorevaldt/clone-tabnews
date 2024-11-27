50989f37c37f0de20c3e423f76985bda951a8b0a
ygorevaldt <evaldtygor@gmail.com>
2024-11-27 07:19:22 -0300

    refatora nome do teste

--- tests/integration/api/v1/status/get.test.ts
+++ tests/integration/api/v1/status/get.test.ts
@@ -1,12 +1,9 @@
 import { describe, expect, it } from "vitest";
 
 describe("status", () => {
-  it("should return http status code 200", async () => {
+  it("GET to /api/v1/status should return http status code 200", async () => {
     const response = await fetch("http://localhost:3000/api/v1/status");
-
     const responseBody = await response.json();
-
-    console.log(responseBody);
 
     const updatedAtParsed = new Date(responseBody.updated_at).toISOString();
 
